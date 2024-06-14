47eff764501195187e5874c327b069dc1cbb7a48
brauliopersz <brauliopersz@gmail.com>
2024-06-14 15:12:15 -0400

    Cambio de algunas palabras

--- src/pages/Auth/ChooseRegister.jsx
+++ src/pages/Auth/ChooseRegister.jsx
@@ -17,7 +17,7 @@ export default function ChooseRegister() {
                     <div className="bg-blackMain mr-2 p-8 w-full h-full">
                         <div className="flex items-center justify-around">
                             <ChooseRegisterCard Icon={FaUserPlus} title={'Usuario de la música'} NameUser={'usuario'} subTitle={'Escucho o busco nuevos talentos.'} img={'https://cdn.pixabay.com/photo/2024/04/12/17/40/model-8692324_960_720.jpg'}/>
-                            <ChooseRegisterCard Icon={FaUserEdit} title={'Creador de música'} NameUser={'artista'} subTitle={'Escribo, compongo o arreglo música.'} img={'https://cdn.pixabay.com/photo/2024/04/17/16/33/ai-generated-8702438_960_720.png'}/>
+                            <ChooseRegisterCard Icon={FaUserEdit} title={'Creador de música'} NameUser={'creador'} subTitle={'Escribo, compongo, arreglo o administro música.'} img={'https://cdn.pixabay.com/photo/2024/04/17/16/33/ai-generated-8702438_960_720.png'}/>
                         </div>
                         <Footer />
                     </div>
