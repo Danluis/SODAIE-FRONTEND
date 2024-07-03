c8acbb29447f7f1472d24867f0d4e87b055f1066
Marcos <2022-0104@itla.edu.do>
2024-07-03 10:36:32 -0400

    creación de la pagina de artistas

---
+++ src/pages/Artist/ArtistPage.jsx
@@ -0,0 +1,23 @@
+import Header from "../../components/Home/Header";
+import Footer from "../../components/Footer";
+import Navbar from "../../components/Home/Navbar";
+import ArtistList from "../../components/Home/ArtistList";
+
+export default function ArtistPage() {
+  return (
+    <div className="w-full h-full max-w-full-xl mt-2 bg-blackMain">
+      <Header />
+      <div className="flex flex-col-2">
+        <Navbar />
+        <div className="w-full h-full">
+          <div className='bg-blackMain mr-2 p-8 w-full h-full text-white mt-10'>
+            <ArtistList title={'Artistas Populares'} />
+            <ArtistList title={'Artistas Nuevos'} />
+            <ArtistList title={'Tus Artistas Favoritos'} />
+            <Footer />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
