a86ab0c89c1cbc9de3fd5d9a4635dd5e6a640f5f
Marcos <2022-0104@itla.edu.do>
2024-07-03 10:45:03 -0400

    Cambio de artistas a Compositores

--- src/pages/Home/HomePageV1.jsx
+++ src/pages/Home/HomePageV1.jsx
@@ -19,7 +19,7 @@ export default function HomePageV1(){
             <div className="w-full h-full">
                 <div className='bg-blackMain mr-2 p-8 w-full h-full text-white'>
                 <Slider/>
-                    <ArtistList title={'Artistas'}/>
+                    <ArtistList title={'Compositores'}/>
                     <SongCardList title={'Escuchado recientemente'}/>
                     <SongCardList title={'Subidas recientemente'}/>
                     <SongCardList title={'Las mas votadas'}/>
