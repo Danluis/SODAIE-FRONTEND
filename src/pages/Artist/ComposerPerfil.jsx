74c68ec837f63b5f4d2a9ef74cb8b4f3fbe02252
Marcos <2022-0104@itla.edu.do>
2024-07-08 11:43:17 -0400

    Pagina estructurada

--- src/pages/Artist/ComposerPerfil.jsx
+++ src/pages/Artist/ComposerPerfil.jsx
@@ -1,9 +1,7 @@
 import Header from "../../components/Home/Header"
 import Footer from "../../components/Footer"
 import Navbar from "../../components/Home/Navbar"
-import CardPlayButton from "../../components/MediaPlayer/CardPlayButton"
-import { Link } from 'react-router-dom'; // Importar Link desde React Router
-export default function ComposerPerfil(){
+export default function Artist(){
 
     return (
         <div className=" w-full h-full max-w-full-xl mt-2 bg-blackMain">
@@ -15,26 +13,7 @@ export default function ComposerPerfil(){
 
             <div className="w-full h-full">
                 <div className='bg-blackMain mr-2 p-8 w-full h-full text-white mt-10'>
-                <div class="w-full h-full max-w-full-xl mt-2 bg-blackMainmax-w-sm bg-gray-800 rounded-lg shadow-lg overflow-hidden">
-        <div class="p-6">
-            <h1 class="text-2xl font-bold text-white mb-2">asdasdasdasdasd</h1>
-            <p class="text-sm text-gray-400 mb-12">
-                <Link to="/Perfil" className="hover:text-white">Aasdasdasdrtistaasdasdasd Compositores</Link> • Año de laasdasdasd canción</p>
-            <div class="flex justify-between items-center mb-4">
-                <button class="relative top-16 z-20 left-14 ">
-                    <CardPlayButton/>
-                </button>
-            </div>
-            <div class="border-t border-gray-700 pt-2 mb-4">
-                <div class="flex justify-between text-gray-400">
-                    <span>Canción</span>
-                    <span>Duración de la canción</span>
-                </div>
-            </div>
-            <p class="text-xs text-gray-400 mb-2">Fecha de publicacion</p>
-            <p class="text-xs text-gray-400">Sus derechos de autor</p>
-        </div>
-    </div>
+
                     <Footer/>
                 </div>
             </div>
