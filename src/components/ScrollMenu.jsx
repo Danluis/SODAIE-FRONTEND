4f4ee24b224d400073716d1344f04ff86e05e9a5
Fyxr17 <2022-0104@itla.edu.do>
2024-07-18 11:11:44 -0400

    creación del componente del scrollmenu

---
+++ src/components/ScrollMenu.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function FormSelectDesigner({ text, options }) {
+    return (
+        <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-400 mb-2">{text}</label>
+            <select className="w-[28rem] px-6 py-3 rounded-lg mt-1 bg-semiBlack border-blue-600 text-white h-40 overflow-y-scroll">
+                {options.map((option, index) => (
+                    <option key={index} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+}
